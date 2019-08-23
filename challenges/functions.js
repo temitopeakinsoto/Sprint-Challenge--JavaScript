6225d681d1e0b0031f40cf1a8eeede6cc6c28a0a
Temitope Akinsoto <takinsoto@gmail.com>
2019-08-23 09:26:08 +0100

    feat: create a higher-order function that accepts callback
    In this commit, i created a higher order function that accepts two params and a callBack
    three callBack function were also created: add() multiply() and greeting()
    reach created callback function takes two parameters
    add() takes two numbers and returns their sum
    multiply() takes two numbers and returns their product
    greeting() takes first_name and last_name and returns a greeting with the full name
    
    CALLBACKS - function.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(param1, param2, callBack) {
+  return callBack(param1, param2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,6 +18,18 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function greeting(firstName, lastName) {
+  let full_name = firstName + " " + lastName;
+  return "Hello, " + full_name + ", nice to meet you!";
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
