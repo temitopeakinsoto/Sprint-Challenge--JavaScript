916687e94de5b11f3065b98b0b3878b7a1bcd7cd
Temitope Akinsoto <takinsoto@gmail.com>
2019-08-23 10:33:18 +0100

    feat: Display both Scientific and Animal name
    In this method,using the forEach(), i created an array of objects from the zooAnimals array
    the objects had two properties Name which was set to the scienitific name of the zooAnimal and Scientific which was set to the scientific name of the animal
    
    ADVANCED ARRAY METHODS - bojects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -123,10 +123,18 @@ const zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_
 
 /* Request 1: .forEach()
 
-The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
+The zoo wants to display both the scientific name and the animal name in front of the habitats.  
+Return an array with only the animal and scientific names in it.  
+The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const animalNames = [];
+let animalNames = [];
+zooAnimals.forEach(function(animal){
+  animalNames.push(
+    {Name: animal.animal_name, 
+      Scientific: animal.scientific_name
+    });
+});
 console.log(animalNames);
 
 /* Request 2: .map()    
